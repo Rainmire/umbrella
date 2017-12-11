55b17d6e489141502fc6af31739674ea97a7e249
Lily Yang <zq.yang.lily@gmail.com>
2017-12-10 16:43:26 -0800

    g log in form works safari pops

---
+++ umbrella_RN_frontend/App.js
@@ -0,0 +1,22 @@
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
+import { Provider } from 'react-redux';
+import SessionReducer from './reducers/session_reducer';
+import LoginForm from './components/login_form';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(SessionReducer, applyMiddleware(logger))}>
+        <View>
+          <LoginForm />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
