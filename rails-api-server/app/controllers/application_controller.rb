475707505a2a3a3260f82d497ddc45f716c61f3b
Qiumin Yin <mqyin@g.ucla.edu>
2017-12-11 18:21:16 -0800

    return oath_token

--- rails-api-server/app/controllers/application_controller.rb
+++ rails-api-server/app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:oauth_token]) if session[:oauth_token]
+  end
+
+  def login(user)
+    session[:oauth_token] = user.oauth_token
+    user.save!
+    @current_user = user
   end
 end
