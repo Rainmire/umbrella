2e20515f8e196dec2d47d380aa9ed623230e4892
Mitch <mqyin@g.ucla.edu>
2017-12-12 11:08:17 -0800

    add user-child associations

--- rails-api-server/app/models/child.rb
+++ rails-api-server/app/models/child.rb
@@ -1,2 +1,8 @@
 class Child < ApplicationRecord
+  validates :name, :teacher_id, :parent_id
+
+  belongs_to :parent,
+  class_name: "User",
+  primary_key: :id,
+  foreign_key: :parent_id
 end
--- rails-api-server/app/models/user.rb
+++ rails-api-server/app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  validates :provider, :name, :type, :oauth_token, :oauth_expires_at
+  validates :email, uniqueness: true
+
+  has_many :children,
+  class_name: "Child",
+  primary_key: :id,
+  foreign_key: :parent_id
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
