4b2db1210d1b2ccbf0fede7a97a652e18d3ff23b
Mitch <mqyin@g.ucla.edu>
2017-12-13 11:47:38 -0800

    make user single controller

--- rails-api-server/config/routes.rb
+++ rails-api-server/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     # get '/user', to: 'users#show', as: 'user'
-    resources :users, only: [:show]
+    resource :user, only: [:show]
     resource :session, only: [:destroy]
     # delete 'session', to: 'sessions#destroy'
   end
