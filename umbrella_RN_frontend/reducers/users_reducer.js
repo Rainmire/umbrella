50f3bc3289f39b2a491020e4613a730fdb49f98b
Lily Yang <zq.yang.lily@gmail.com>
2017-12-14 09:54:55 -0800

    state passed down

--- umbrella_RN_frontend/reducers/users_reducer.js
+++ umbrella_RN_frontend/reducers/users_reducer.js
@@ -5,8 +5,8 @@ import merge from 'lodash/merge';
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
-    // case RECEIVE_PICS:
-    //   return action.pictures;
+    case RECEIVE_USERS:
+      return merge({},state,action.users);
     // case RECEIVE_PIC:
     //   return merge({},state,{[action.picture.id]:action.picture});
     // case REMOVE_PIC:
