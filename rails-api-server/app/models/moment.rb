1cf7a00602136126b8967a261100813a7e2a5f51
Mitch <mqyin@g.ucla.edu>
2017-12-13 18:08:47 -0800

    add moments associations

--- rails-api-server/app/models/moment.rb
+++ rails-api-server/app/models/moment.rb
@@ -1,2 +1,10 @@
 class Moment < ApplicationRecord
+  validates :body, :author_id, :child_id, presence: true
+
+  belongs_to :author,
+  class_name: :User,
+  foreign_key: :author_id
+
+  belongs_to :child
+
 end
