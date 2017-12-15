e7587f5cb60c83aea1c2627fc02efa64b9f82b2e
Qiumin Yin <mqyin@g.ucla.edu>
2017-12-15 09:08:32 -0800

    stashed

--- umbrella_RN_frontend/components/moments_form_container.js
+++ umbrella_RN_frontend/components/moments_form_container.js
@@ -2,9 +2,11 @@ import { connect } from 'react-redux';
 import MomentsForm from './moments_form';
 
 const mapSTPs = (state) => ({
-
-})
+  students: state.
+});
 
 const mapDTPs = (dispatch) => ({
 
-})
+});
+
+export default connect(mapSTPs, mapDTPs)(MomentsForm);
