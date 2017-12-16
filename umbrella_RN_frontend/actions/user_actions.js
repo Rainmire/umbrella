afa75d7d38c4bd63ccfe78efcc214882dd174faf
Lily Yang <zq.yang.lily@gmail.com>
2017-12-15 20:30:43 -0800

    fixed current user

--- umbrella_RN_frontend/actions/user_actions.js
+++ umbrella_RN_frontend/actions/user_actions.js
@@ -4,10 +4,16 @@ import { receiveMoments } from './moment_actions';
 export const RECEIVE_CHILD_INFO = "RECEIVE_CHILD_INFO";
 
 export const RECEIVE_USERS = "RECEIVE_USERS";
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 
 export const receiveUsers = (users) => ({
   type: RECEIVE_USERS,
   users
+});
+
+export const receiveCurrentUser = (user) => ({
+  type: RECEIVE_CURRENT_USER,
+  user
 });
 
 export const fetchCurrentUser = (token) => (dispatch) => {
@@ -17,10 +23,16 @@ export const fetchCurrentUser = (token) => (dispatch) => {
   }).then(({_bodyInit}) => {
     const response = JSON.parse(_bodyInit);
     console.log(response);
+    console.log(Boolean(response.users[response.current_user_id].class));
     dispatch(receiveChildren(response.children));
     dispatch(receiveUsers(response.users));
     dispatch(receiveMoments(response.moments));
-    dispatch(receiveCurrentChild(response.children[0]));
+    const currentUser = response.users[response.current_user_id];
+    if(!currentUser.class){
+      console.log("is parent");
+      dispatch(receiveCurrentChild(response.children[0]));
+    }
+    dispatch(receiveCurrentUser(currentUser));
   });
 };
 
