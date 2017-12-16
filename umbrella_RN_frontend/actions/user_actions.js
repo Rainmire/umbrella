38f4fe09cb1099eeb8f2030af29788ad2c2e9928
Lily Yang <zq.yang.lily@gmail.com>
2017-12-16 14:03:20 -0800

    debugged

--- umbrella_RN_frontend/actions/user_actions.js
+++ umbrella_RN_frontend/actions/user_actions.js
@@ -23,12 +23,12 @@ export const fetchCurrentUser = (token) => (dispatch) => {
   }).then(({_bodyInit}) => {
     const response = JSON.parse(_bodyInit);
     console.log(response);
-    console.log("is teacher?", Boolean(response.users[response.current_user_id].class));
+    console.log("is teacher?", Boolean(response.users[response.current_user_id].teacher_class));
     dispatch(receiveChildren(response.children));
     dispatch(receiveUsers(response.users));
     dispatch(receiveMoments(response.moments));
     const currentUser = response.users[response.current_user_id];
-    if(!currentUser.class){
+    if(!currentUser.teacher_class){
       console.log("is parent");
       dispatch(receiveCurrentChild(response.children[0]));
     }
