09e95a45e7b512b2913530f7c4ea429fadbb3fca
Lily Yang <zq.yang.lily@gmail.com>
2018-01-05 18:06:40 -0800

    added calendar

--- umbrella_RN_frontend/components/calendar_screen.js
+++ umbrella_RN_frontend/components/calendar_screen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { Calendar, CalendarList, Agenda, LocaleConfig } from 'react-native-calendars';
+
 
 class CalendarScreen extends React.Component {
   static navigationOptions = {
@@ -13,15 +15,30 @@ class CalendarScreen extends React.Component {
 
   render() {
     return(
-      <View>
-        <Text style={styles.calendar}>
-           You are now on the calendar view page.
-        </Text>
+      <View style={{ paddingTop: 50, flex: 1 }}>
+        <Calendar
+          current={'2018-01-01'}
+          minDate={'2017-12-10'}
+          maxDate={'2017-12-30'}
+          onDayPress={(day) => {console.log('selected day', day)}}
+          monthFormat={'yyyy MM'}
+          onMonthChange={(month) => {console.log('month changed', month)}}
+          hideArrows={false}
+          // renderArrow={(direction) => (<Arrow />)}
+          hideExtraDays={true}
+          disableMonthChange={true}
+          firstDay={1}
+        />
       </View>
     );
   }
 }
 
+// <View>
+//   <Text style={styles.calendar}>
+//      You are now on the calendar view page.
+//   </Text>
+// </View>
 export default CalendarScreen;
 
 export const styles = StyleSheet.create({
