28b42d69b549bd7bce512402221b0d1068268b9e
Lily Yang <zq.yang.lily@gmail.com>
2018-01-07 17:36:45 -0800

    added reducer

--- umbrella_RN_frontend/reducers/calendars_reducer.js
+++ umbrella_RN_frontend/reducers/calendars_reducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 import EventsReducer from './events_reducer';
-import markedDatesReducer from './marked_dates_reducer';
+import MarkedDatesReducer from './marked_dates_reducer';
 
 export default combineReducers({
   events: EventsReducer,
-  markedDates: markedDatesReducer,
+  markedDates: MarkedDatesReducer,
 });
