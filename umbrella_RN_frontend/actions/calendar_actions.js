1f4524ef1703f13453ea4c50ff0f70cd2ce343f0
Lily Yang <zq.yang.lily@gmail.com>
2018-01-16 14:14:53 -0800

    calendar action working

--- umbrella_RN_frontend/actions/calendar_actions.js
+++ umbrella_RN_frontend/actions/calendar_actions.js
@@ -17,10 +17,9 @@ export const fetchCalendar = (date,token) => dispatch => {
   return fetch('http://localhost:3000/api/calendar/monthly_events',{
     method:'POST',
     headers: { 'Authorization': token },
-    body: JSON.stringify({date:date})
-  }).then((rep)=>{
-    console.log(JSON.parse(rep));
-    // dispatch(receiveEvents(JSON.parse(_bodyInit.events)));
+    body: date
+  }).then(({_bodyInit})=>{
+    dispatch(receiveEvents(JSON.parse(_bodyInit.events)));
     // dispatch(receiveMarkedDates(JSON.parse(_bodyInit.markedDates)));
   });
 };
